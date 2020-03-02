f855ae25421424430867c8e3ab6914b8f92bdcef
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-03-02 17:16:21 +0530

    MODIFIED :pattern of Reset board and choose the valid cell for the player

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class TicTacToe {
 //VARIABLES
@@ -5,21 +8,8 @@ public class TicTacToe {
 	String Letter;
 	String Computer;
 	String toss;
-// METHOD FOR rESETTING THE BOARD
-	void resetBoard()
-	{	System.out.println(" "+"__"+"  "+"__"+"  "+"__");
-		for(int row=1;row<=3;row++)
-		{
-			for(int column=1;column<=3;column++)
-			{ 
-				System.out.print("|");
-				System.out.print("__");
-				System.out.print("|");
-			}
-			System.out.println("");	
-		}
-	}
-	
+	int num_Of_row=3;
+	int num_Of_cols=3;	
 	// ASSIGING A LETTER  X AND O 
 	void assignLetter()
 	{
@@ -48,12 +38,70 @@ public class TicTacToe {
 			System.out.println("Computer will play first :"+Computer);
 		}
 	}
+	// PLAYER WOULD SEE THE BOARD AND CHOICE THE VALID CELL 
+	 public  void resetBoard(char[][]gameboard)
+		{
+				
+			 for(char[]row:gameboard)
+			 {
+				 for (char c :row)
+				 {
+					 System.out.print(c);
+				 }
+				 System.out.println();
+			 }	
+			
+		}
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe");
 		TicTacToe tt=new TicTacToe();
 		tt.assignLetter();
 		tt.toss();
-		tt.resetBoard();
+		char[][]gameboard={
+			 	{' ', '|',' ','|',' '},
+				{'_','+','_','+','_'},
+				{' ', '|',' ','|',' '},
+				{'_','+','_','+','_'},
+				{' ', '|',' ','|',' '}
+			};
+	
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter the number (1-9):");
+	 int position=sc.nextInt();
+	 System.out.println(position);
+	  switch(position)
+	  {
+	  case 1:
+		  gameboard[0][0]='X';
+		  break;
+	  case 2:
+		  gameboard[0][2]='X';
+		  break;
+	  case 3:
+		  gameboard[0][4]='X';
+		  break;
+	  case 4:
+		  gameboard[2][0]='X';
+		  break;
+	  case 5:
+		  gameboard[2][2]='X';
+		  break;
+	  case 6:
+		  gameboard[2][4]='X';
+		  break;
+	  case 7:
+		  gameboard[4][0]='X';
+		  break;
+	  case 8:
+		  gameboard[4][2]='X';
+		  break;
+	  case 9:
+		  gameboard[4][4]='X';
+		  break;
+	  } 
+	  tt.resetBoard(gameboard);
+		
+		
 		
 		
 		
