0c225ef69cf0a7173c921d8f3a794949f11de9fb
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-03-02 11:10:07 +0530

    MODIFIED :Who will Play first that is assigned in toss function

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,6 +3,8 @@ public class TicTacToe {
 //VARIABLES
 	String player;
 	String Letter;
+	String Computer;
+	String toss;
 // METHOD FOR rESETTING THE BOARD
 	void resetBoard()
 	{	System.out.println(" "+"__"+"  "+"__"+"  "+"__");
@@ -24,16 +26,36 @@ public class TicTacToe {
 		if(Letter=="X")
 		{
 			player="X";
+			Computer="O";
 		}
 		else
 		{
 			player="O";
+			Computer="X";
+		}
+		System.out.println("player   : "+player);
+		System.out.println("Computer : "+Computer);
+	}
+	//  TOSS AND WHO WILL PLAY FIRST
+	void toss()
+	{
+		if(toss=="X")
+		{
+			System.out.println("Player will  play first :"+player);
+		}
+		else
+		{
+			System.out.println("Computer will play first :"+Computer);
 		}
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe");
 		TicTacToe tt=new TicTacToe();
+		tt.assignLetter();
+		tt.toss();
 		tt.resetBoard();
+		
+		
 		
 		// TODO Auto-generated method stub
 
