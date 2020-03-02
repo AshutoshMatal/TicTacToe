286e4fb633817a7ff9e822ccd8a617f865fb4daa
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-03-02 10:33:19 +0530

    ADDED : Start with resetting the board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,28 @@
 
 public class TicTacToe {
 
+	void resetBoard()
+	{	System.out.println(" "+"__"+"  "+"__"+"  "+"__");
+		for(int row=1;row<=3;row++)
+		{
+			for(int column=1;column<=3;column++)
+			{ 
+				System.out.print("|");
+				System.out.print("__");
+				System.out.print("|");
+			}
+			//System.out.println("|");
+			System.out.println("");
+			
+		}
+		
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe");
+		TicTacToe tt=new TicTacToe();
+		tt.resetBoard();
+		
 		// TODO Auto-generated method stub
 
 	}
